98be4c6049530c1e5e5e2081196dd8af30a62dc3
William Wood Harter <wharter@luminoso.com>
2019-03-21 16:23:00 -0700

    Added the ability to login with a userid and password

--- luminosoWDC.js
+++ luminosoWDC.js
@@ -146,6 +146,7 @@
     }); // get_metadata
   }; // getSchema
 
+
   /**
    * Add url parameters
    *
@@ -170,6 +171,44 @@
       encodeURIComponent(value);
     return url;
   }
+
+function luminoso_login(proj_api_v4, lumi_loginid, lumi_password, login_callback) {
+  console.log("api url=" + proj_api_v4);
+
+  /*
+  // create the url object
+  var url = proj_api_v4 + "/users/login/";
+  url = addParameterToURL(url,"username",lumi_loginid);
+  url = addParameterToURL(url,"password",lumi_password);
+
+  console.log("login url=" + url);
+
+  $.ajax({
+    url: url,
+    type: "GET",
+    dataType: "json",
+    success: function(resp_data) {
+      console.log("login SUCCESS");
+      console.log("login response = "+resp_data);
+      login_callback(resp_data);
+    },
+    error: function(xhr, status, text) {
+      console.log("ERROR getting metadata: " + status);
+      console.log("error text = " + text);
+
+      var response = $.parseJSON(xhr.responseText);
+      if (response) console.log(response.error);
+    },
+    beforeSend: setHeader
+  });
+
+  function setHeader(xhr) {
+    xhr.setRequestHeader("lumi", "lumi-cors");
+  }
+  */
+ 
+} // luminoso login
+
 
   /**
    * Get project metadata
@@ -663,50 +702,162 @@
 })(); // register connectionName
 
 $(document).ready(function() {
+
+
   $("#submitButton").click(function() {
+
+    var lumi_url_tmp = $("#lumi-project-url").val().trim();
+    var lumi_token_tmp = $("#lumi-token").val();
+    var lumi_username_tmp = $("#lumi-username").val();
+    var lumi_password_tmp = $("#lumi-password").val();
+
+    // DEBUG:
+    // Test urls, these are much faster when testing!
+    // lumi_url_tmp = "https://analytics.luminoso.com/app/projects/p87t862f/prk3wg56"
+    // lumi_token_tmp = "0Cr7-TIYLTEsynXW1wFiHTAOsUlUFX2h";
+    // lumi_url_tmp = "http://localhost:8889/analytics.luminoso.com/app/projects/p87t862f/prk3wg56"
+    // lumi_url_tmp = "https://analytics.luminoso.com/app/projects/p87t862f/prk3wg56"
+    // lumi_url_tmp= "https://analytics.luminoso.com/app/projects/p87t862f/prsfdrn2";
+
+    // https://analytics.luminoso.com/app/projects/p87t862f/prsfdrn2
+    // "0Cr7-TIYLTEsynXW1wFiHTAOsUlUFX2h"
+
+    // convert an app url into an api url
+    project_url = lumi_url_tmp;
+    api_url = project_url.split("/app")[0] + "/api/v5";
+    api_v4_url = project_url.split("/app")[0] + "/api/v4";
+    url_arr = project_url.split("/");
+    project_id = url_arr[url_arr.length - 1];
+    account_id = url_arr[url_arr.length - 2];
+    proj_apiv4 = api_url + "/projects/" + account_id + "/" + project_id;
+    proj_apiv5 = api_url + "/projects/" + project_id;
+
+    // prepend the cors proxy until this is hosted on luminoso.com
+    // proxy_url = "https://cors-anywhere.herokuapp.com/";
+    // proxy_url = "http://localhost:8080/";
+    var proxy_url = "https://morning-anchorage-77576.herokuapp.com/";
+    api_url = proxy_url+api_url;
+    api_v4_url = proxy_url+api_v4_url;
+    proj_apiv5 = proxy_url + proj_apiv5;
+    proj_apiv4 = proxy_url + proj_apiv4;
+
+    // if lumi_token isn't set, then probably came through the username way
+    if (!lumi_token_tmp)
+    {
+      if (lumi_username_tmp)
+      {
+        lumi_login(api_v4_url,lumi_username_tmp,lumi_password_tmp,function(token) {
+          tableau.log("GOT THE TOKEN!!!!");
+          tableau.log("token="+JSON.stringify(token));
+          lumi_token_tmp = token;
+
+          tableau_submit(proj_apiv5,lumi_token_tmp);
+        });
+      }
+      else
+      {
+        tableau.log("ERROR no token and no username. One or other must be set");
+      }
+    }
+    else
+    {
+      tableau_submit(proj_apiv5,lumi_token_tmp);
+    }
+
+
+  }); // submit button click
+
+  /**
+   * Calls the tableau submit.
+   * 
+   * Description.
+   * This starts the whole process. It was split into a function because the
+   * login process is async and calls it after it gets a token and if a token
+   * is given in the UI then it simply calls this directly to kick off the connector
+   * 
+   * @param {*} lumi_project_url - The Luminoso Daylight v5 project api url
+   * @param {*} lumi_token  - The security token to use
+   */
+
+  function tableau_submit(lumi_project_url,lumi_token) {
+
     var lumiDataObj = {
-      lumi_url: $("#lumi-project-url")
-        .val()
-        .trim(),
-      lumi_token: $("#lumi-token")
-        .val()
-        .trim()
+      lumi_url: lumi_project_url,
+      lumi_token: lumi_token
     };
 
     tableau.log("lumi-url=" + lumiDataObj.lumi_url);
 
-    // DEBUG:
-    // Test urls, these are much faster when testing!
-    // lumiDataObj.lumi_url = "https://analytics.luminoso.com/app/projects/p87t862f/prk3wg56"
-    // lumiDataObj.lumi_token = "gVgsZAq03A6U6f3ZF9K4s2HJFFJmFFjt";
-    //lumiDataObj.lumi_url = "http://localhost:8889/analytics.luminoso.com/app/projects/p87t862f/prk3wg56"
-    //lumiDataObj.lumi_url = "https://analytics.luminoso.com/app/projects/p87t862f/prk3wg56"
-    // lumiDataObj.lumi_url = "https://analytics.luminoso.com/app/projects/p87t862f/prsfdrn2";
 
-    // https://analytics.luminoso.com/app/projects/p87t862f/prsfdrn2
-    //lumi_token = "gVgsZAq03A6U6f3ZF9K4s2HJFFJmFFjt"
-
-    // convert an app url into an api url
-    project_url = lumiDataObj.lumi_url;
-    api_url = project_url.split("/app")[0] + "/api/v5";
-    url_arr = project_url.split("/");
-    project_id = url_arr[url_arr.length - 1];
-    account_id = url_arr[url_arr.length - 2];
-    proj_apiv5 = api_url + "/projects/" + project_id;
-    lumiDataObj.lumi_url = proj_apiv5;
-
-    // prepend the cors proxy until this is hosted on luminoso.com
-    //lumiDataObj.lumi_url = "https://cors-anywhere.herokuapp.com/" + lumiDataObj.lumi_url;
-    //lumiDataObj.lumi_url = "http://localhost:8080/" + lumiDataObj.lumi_url;
-    lumiDataObj.lumi_url =
-      "https://morning-anchorage-77576.herokuapp.com/" + lumiDataObj.lumi_url;
     tableau.connectionData = JSON.stringify(lumiDataObj);
     tableau.connectionName = "Luminoso Data";
     tableau.submit();
 
     tableau.log("a SUBMIT PRESSED!!");
+  } // tableau submit
 
-    //tableau.connectionName = "USGS Earthquake Feed";
-    //tableau.submit();
-  });
-});
+  /**
+   * Login to Luminoso Daylight and receive a token
+   * 
+   * Description
+   * This uses a v4 endpoint to login to the Daylight server and get a token.
+   * The token will be used for the rest of the connections
+   * 
+   * @param {*} proj_api_v4 | Login requires a v4 endpoint
+   * @param {*} lumi_loginid | The login id
+   * @param {*} lumi_password | The password the user typed at the prompt
+   * @param {*} login_callback | The callback when this async function is called
+   */
+  function lumi_login(proj_api_v4,lumi_loginid,lumi_password,login_callback)
+  {
+    tableau.log("LOGIN START...")
+
+    // create the url object
+    var url = proj_api_v4 + "/user/login/";
+    var params = {
+      username: lumi_loginid,
+      password: lumi_password
+    };
+    tableau.log("login url=" + url);
+
+    $.ajax({
+      url: url,
+      type: "POST",
+      dataType: "json",
+      data:params,
+      success: function(resp_data) {
+        tableau.log("login SUCCESS");
+        tableau.log("login response = "+resp_data);
+        login_callback(resp_data['result']['token']);
+      },
+      error: function(xhr, status, text) {
+        tableau.log("ERROR on login: " + status);
+        tableau.log("error text = " + text);
+        tableau.log("full error ="+JSON.stringify(xhr));
+
+        var response = $.parseJSON(xhr.responseText);
+        if (response) tableau.log(response.error);
+      },
+      beforeSend: setHeader
+    });
+
+    function setHeader(xhr) {
+      xhr.setRequestHeader("lumi", "lumi-cors");
+    }
+
+    function addParameterToURL(url, param, value) {
+      // console.log("addparam url=" + url);
+      tableau.log("encode of value for "+String(param)+" = "+String(encodeURIComponent(value)))
+      url =
+        url +
+        (url.split("?")[1] ? "&" : "?") +
+        param +
+        "=" +
+        encodeURIComponent(value);
+      return url;
+    }
+    
+  }
+}); // document ready
+
+
